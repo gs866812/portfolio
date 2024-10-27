03eccba0516bd7c1e747a7d5859bd04aa5b99a3e
gs866812 <gooogle.sarwar@gmail.com>
2024-10-27 20:19:18 +0600

    first commit

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <div className='hidden lg:w-[415px] lg:h-screen lg:flex bg-[#444444]'>
+        <div className='w-[100px] bg-[#222222]'>
+            <div className='border-b-[.5px] h-[80px]'></div>
+            <div className='border-b-[.5px] h-[80px]'></div>
+        </div>
+
+        <div className='w-full'>
+
+        </div>
+    </div>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>
+        Main
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
